5553357f5fad4dbea9cf85a47e906b1e9e42cbdb
qian2z <98630339+qian2z@users.noreply.github.com>
2023-12-27 15:03:08 +0800

    Build NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,54 +3,63 @@ import { Container, Flex, Text } from "@radix-ui/themes";
 import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+import { IoMdPerson } from "react-icons/io";
 import { TfiWrite } from "react-icons/tfi";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   return (
-    <nav className="border-b mb-5 px-5 h-14 py-4">
+    <nav className="border-b mb-5 px-5 h-14 py-2">
       <Container>
-        <Flex>
-          <Flex gap="4">
+        <Flex gap="4" justify="between">
+          <NavLink
+            link={{ label: "About Us", href: "/about-us" }}
+            currentPath={currentPath}
+          />
+          <Flex mt="2">
             <Link href="/">
               <Flex gap="3" justify="center" align="center">
-                <TfiWrite />
+                <TfiWrite size={25} />
                 <Text weight="bold" size="5">
                   AutoGrader
                 </Text>
               </Flex>
             </Link>
-            <NavLinks />
           </Flex>
+          <NavLink
+            link={{ label: "Login", href: "/login", icon: <IoMdPerson /> }}
+            currentPath={currentPath}
+          />
         </Flex>
       </Container>
     </nav>
   );
 };
 
-const NavLinks = () => {
-  const currentPath = usePathname();
-  const links = [
-    { label: "Home", href: "/" },
-    { label: "Get Started", href: "/started" },
-  ];
-
+const NavLink = ({
+  link,
+  currentPath,
+}: {
+  link: { label: string; href: string; icon?: ReactNode };
+  currentPath: string;
+}) => {
   return (
-    <ul className="flex space-x-3">
-      {links.map((link) => (
-        <li key={link.label}>
-          <Link
-            className={classnames({
-              "nav-link": true,
-              "!text-zinc-900 font-semibold bg-sky-100":
-                link.href === currentPath,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <Flex>
+      <Link
+        href={link.href}
+        className={classnames({
+          "nav-link": true,
+          "!text-zinc-900 font-semibold bg-sky-100": link.href === currentPath,
+        })}
+      >
+        <Flex gap="2" justify="center" align="center">
+          <Text>{link.label}</Text>
+          {link?.icon}
+        </Flex>
+      </Link>
+    </Flex>
   );
 };
 
