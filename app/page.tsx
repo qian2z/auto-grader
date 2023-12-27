6bb1da601a1e68fe643a2ad064435b095c2689bf
qian2z <98630339+qian2z@users.noreply.github.com>
2023-12-27 15:09:02 +0800

    Build Get Started Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import { GiClick } from "react-icons/gi";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Button>
+      <Link href="/get-started">
+        <Flex gap="3" justify="center" align="center">
+          <Text>Get Started</Text>
+          <GiClick />
+        </Flex>
+      </Link>
+    </Button>
+  );
 }
