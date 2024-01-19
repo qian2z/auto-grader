13552f81dc6e4c7564292b77b782279169ec562a
qian2z <98630339+qian2z@users.noreply.github.com>
2024-01-19 13:46:58 +0800

    Require Get-Started to Have User Logged In

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
+    "/get-started",
     "/single-submission",
     "/multiple-submissions",
     "/results-loading",
