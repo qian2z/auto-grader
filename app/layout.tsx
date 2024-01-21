37cc8e7692d9804cd845b31509ab655ec6495563
qian2z <98630339+qian2z@users.noreply.github.com>
2024-01-21 22:11:35 +0800

    Implement Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "./Footer";
@@ -42,6 +43,7 @@ export default function RootLayout({
           </AuthProvider>
         </QueryClientProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
