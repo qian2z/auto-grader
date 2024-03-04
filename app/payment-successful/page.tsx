474bea59ea80f78f42896bf456ea37b6d2cdf9f1
qian2z <98630339+qian2z@users.noreply.github.com>
2024-03-05 03:27:36 +0800

    Fix Error

--- app/payment-successful/page.tsx
+++ app/payment-successful/page.tsx
@@ -2,19 +2,17 @@
 import PaymentSuccessfulPage from "@/pages/PaymentSuccessfulPage";
 import RequestTimeoutErrorPage from "@/pages/RequestTimeoutErrorPage";
 import axios from "axios";
+import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const PaymentSuccessful = () => {
-  const params = new URLSearchParams(window.location.search);
-  const sessionId = params.get("sessionId");
+  const params = useSearchParams();
+  const sessionId = params?.get("sessionId");
 
   useEffect(() => {
     const fetchSession = async () => {
       try {
-        const response = await axios.post(
-          "/api/getPayment",
-          JSON.stringify({ id: sessionId })
-        );
+        await axios.post("/api/getPayment", JSON.stringify({ id: sessionId }));
       } catch (error) {
         return <RequestTimeoutErrorPage />;
       }
