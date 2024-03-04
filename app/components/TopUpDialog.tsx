b43a054d1b2fc963ddc9cbade704db84c75dfcb5
qian2z <98630339+qian2z@users.noreply.github.com>
2024-03-05 04:16:32 +0800

    Fix Styling Bug

--- app/components/TopUpDialog.tsx
+++ app/components/TopUpDialog.tsx
@@ -28,7 +28,7 @@ const TopUpDialog = () => {
   return (
     <DialogRoot>
       <DialogTrigger>
-        <Button size="1" color="orange" className="custom-button">
+        <Button size="1" color="yellow" variant="classic">
           Top Up <BiSolidPurchaseTag />
         </Button>
       </DialogTrigger>
