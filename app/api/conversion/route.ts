f0b295b34b3d43ff64b10845d306df4f7acbedbf
qian2z <98630339+qian2z@users.noreply.github.com>
2024-03-22 13:05:08 +0800

    Update Conversion Endpoint

--- app/api/conversion/route.ts
+++ app/api/conversion/route.ts
@@ -8,15 +8,23 @@ export async function POST(request: NextRequest) {
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const formData = await request.formData();
-  const convertApiResponse = await axios.post(
-    `https://v2.convertapi.com/convert/pdf/to/docx?Secret=${process.env
-      .NEXT_PUBLIC_CONVERT_API_SECRET!}&StoreFile=true`,
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
-  return NextResponse.json(convertApiResponse.data, { status: 201 });
+
+  try {
+    const convertApiResponse = await axios.post(
+      `https://v2.convertapi.com/convert/pdf/to/docx?Secret=${process.env
+        .NEXT_PUBLIC_CONVERT_API_SECRET!}&StoreFile=true`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return NextResponse.json(convertApiResponse.data, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
